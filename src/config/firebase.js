e7032eaac586fdc51ddafb0bfb720130f9114a7c
Dusan <dusanjankovic877@gmail.com>
2022-04-26 17:54:18 +0200

    initializeFirebaseApp()

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apiKey: "AIzaSyAUP1ivgscH77FaWcyRr2MOpPMJ-nTOrso",
+    authDomain: "vueschoolform-92a62.firebaseapp.com",
+    projectId: "vueschoolform-92a62",
+    storageBucket: "vueschoolform-92a62.appspot.com",
+    messagingSenderId: "253103534127",
+    appId: "1:253103534127:web:41b32a1cd57debe4a8d6da"
+  }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import firebaseConfig from '@/config/firebase'
+import { initializeApp } from "firebase/app"
+initializeApp(firebaseConfig);
 createApp(App).mount('#app')
